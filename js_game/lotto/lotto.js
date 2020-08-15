506d8fcb8dcf9d69780a4418bcc9788c07e3ab88
xmaswith <xmaswith@gmail.com>
2020-08-15 17:08:13 +0800

    Update lotto.js

--- js_game/lotto/lotto.js
+++ js_game/lotto/lotto.js
@@ -11,9 +11,57 @@ while (candidate.length > 0) {
 }
 
 console.log(shuffle)
-const winBalls = shuffle.slice(0, 6)
+const winBalls = shuffle.slice(0, 6).sort((p, c) => p - c)
 const bonus = shuffle[6]
 console.log (`${winBalls} + ${bonus}`)
 
-result.textContent = winBalls
-bonusResult.textContent = bonus
+/*result.textContent = winBalls
+bonusResult.textContent = bonus*/
+
+
+//함수 만들어서 반복때 쓰기
+function colorize(number, chance) {
+    const ball = document.createElement('div');
+    ball.className = 'ball';
+    if (number < 10) { ball.style.backgroundColor = 'red' }
+    else if (number < 20) { ball.style.backgroundColor = 'orange' }
+    else if (number < 30) { ball.style.backgroundColor = 'yellow' }
+    else if (number < 40) { ball.style.backgroundColor = 'green' }
+    else { ball.style.backgroundColor = 'skyblue' }
+    ball.textContent = number;                                     
+    chance.appendChild(ball);    
+}
+
+
+
+
+for (let i = 0; i < 6; i++) {                                           //공 개수만큼 반복
+    /*const ball = document.createElement('div');                         //div 태그 만들기        +       css로 공 모양 만들어 놓음
+    ball.className = 'ball';                                            //div 태그 클래스명 설정    
+    if (winBalls[i] < 10) { ball.style.backgroundColor = 'red' }
+    else if (winBalls[i] < 20) { ball.style.backgroundColor = 'orange' }
+    else if (winBalls[i] < 30) { ball.style.backgroundColor = 'yellow' }
+    else if (winBalls[i] < 40) { ball.style.backgroundColor = 'green' }
+    else { ball.style.backgroundColor = 'skyblue' }
+
+    ball.textContent = winBalls[i];                                     //div 안에 로또 번호 추가
+    result.appendChild(ball);                                           //html 에서 설정한 result div 안에 공 넣기*/
+
+    setTimeout(() => {
+        colorize(winBalls[i], result);  //로또 당첨 번호에 함수 불러오기
+    }, 1000 * (i + 1));    
+}
+
+/*const ball = document.createElement('div');
+ball.className = 'ball';
+ball.textContent = bonus ;
+bonusResult.appendChild(ball);*/
+
+setTimeout(() => {
+    colorize(bonus, bonusResult);       //보너스 공에 함수 불러오기
+}, 7000);
+
+
+winBalls.forEach((number, index) => {       //forEach 반복문. 배열 map 하고 구조 같다. 
+                                            //return 있으면 map, 없으면 forEach 쓰자. 간단하게 생각해서
+})
